9945fd180f9fda1288feb309a7a1d499d1566461
Neil Sarkar <neil.r.sarkar@gmail.com>
2022-02-02 00:48:01 -0500

    Make clearer stories for PinkButton

--- tailwind/src/components/PinkButton.stories.tsx
+++ tailwind/src/components/PinkButton.stories.tsx
@@ -3,36 +3,21 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { PinkButton } from './PinkButton';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: PinkButton,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  title: 'Example/PinkButton',
+  component: PinkButton
 } as ComponentMeta<typeof PinkButton>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof PinkButton> = (args) => <PinkButton {...args} />;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  label: 'Button',
+export const Big = Template.bind({});
+Big.args = {
+  label: 'Big Button',
+  size: 'big'
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  label: 'Button',
+export const Little = Template.bind({});
+Little.args = {
+  label: 'Little Button',
+  size: 'little'
 };
--- tailwind/src/components/PinkButton.tsx
+++ tailwind/src/components/PinkButton.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { isWhiteSpaceLike } from 'typescript';
 
 interface PinkButtonProps {
-	children: React.ReactChildren,
-	label: string
+	label: string,
+	size: 'big' | 'little'
 }
 
 export const PinkButton = ({
-	children,
-	label
+	label,
+	size
 } : PinkButtonProps) => {
 	return (
-		<button aria-label={label} className="bg-pink-500 text-white border border-white">
-			{children}
+		<button aria-label={label} style={
+			{
+				backgroundColor: 'hotpink',
+				color: 'white',
+				border: '1px solid white',
+				width: size === 'big' ? '100vw' : '50px',
+				padding: size === 'big' ? '10vw' : '2px',
+				fontSize: size === 'big' ? '72px' : '12px'
+			}
+		}>
+			{label}
 		</button>
 	);
 }
